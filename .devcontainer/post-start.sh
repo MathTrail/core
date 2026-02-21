c275fc42d0a0e094f4c80b75765fa47c3e3604ef
RyazanovAlexander <alexander.ryazanov.1987@gmail.com>
2026-02-21 09:37:09 -0600

    Update post-start.sh

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -5,7 +5,7 @@ set -e
 set -a; source /etc/mathtrail/platform.env; set +a
 
 # Set up skaffold.env
-[ -f /etc/mathtrail/platform.env ] && ln -sf /etc/mathtrail/platform.env "$PWD/skaffold.env"
+ln -sf /etc/mathtrail/platform.env "$PWD/skaffold.env"
 
 # Set up kubeconfig from host bind mount (mount is root-owned, hence sudo)
 mkdir -p /home/vscode/.kube
